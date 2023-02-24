8c6c71c68b01426ec885a7547c1f27b504fb927d
Christian Alonso <christianalonso.r10@gmail.com>
2023-02-24 17:14:19 +0100

    galleryDetail

--- ROYALE_CLASH/src/components/getCharacter/getCharacter.jsx
+++ ROYALE_CLASH/src/components/getCharacter/getCharacter.jsx
@@ -6,7 +6,9 @@ const GetCharacter = ({ character }) => {
   return (
     <figure className="cr-figure">
       <img src={character.iconUrls.medium} alt={character.name} />
-      <p>{character.name}</p>
+      <p className="cr-p1">{character.name}</p>
+      <p className="cr-p2">Level {JSON.stringify(character.maxLevel)}</p>
+      <p className="cr-p3">Elixir cost {character.elixir}</p>
     </figure>
   );
 };
