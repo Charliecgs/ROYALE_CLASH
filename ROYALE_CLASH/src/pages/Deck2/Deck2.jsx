c0d8b2e8abc69546d128854689d2c34185ed9947
Charliecgs <cgsmanu@gmail.com>
2023-02-25 16:35:33 +0100

    botones paginas

--- ROYALE_CLASH/src/pages/Deck2/Deck2.jsx
+++ ROYALE_CLASH/src/pages/Deck2/Deck2.jsx
@@ -1,4 +1,4 @@
-import './Deck.css';
+import './Deck2.css';
 
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
@@ -7,42 +7,42 @@ import { NavLink } from 'react-router-dom';
 import useDebounce from '../../hook/useDebonced';
 import MainGallery from '../../layouts/MainGallery';
 
-const Deck = () => {
-  const [filterClash, setFilterClash] = useState([]);
-  const [royal, setRoyal] = useState([]);
+const Deck2 = () => {
+  const [filterClash2, setFilterClash2] = useState([]);
+  const [royal2, setRoyal2] = useState([]);
   const [loaded, setLoaded] = useState(false);
   const [loaded2, setLoaded2] = useState(false);
-  const [deck, setDeck] = useState([]);
+  const [deck2, setDeck2] = useState([]);
 
-  const debouncedValue = useDebounce(filterClash, 1000);
+  const debouncedValue2 = useDebounce(filterClash2, 1000);
 
-  const getClash = async () => {
+  const getClash2 = async () => {
     const res = await axios.get('https://63f74109e40e087c958aaa97.mockapi.io/items');
     const data = res.data[0].items;
-    setRoyal(data);
-    setFilterClash(data);
+    setRoyal2(data);
+    setFilterClash2(data);
     setLoaded(true);
   };
 
   const charactersFilter = (key) => {
-    const filter = royal.filter(
+    const filter = royal2.filter(
       (item) =>
         item.name.toLowerCase().includes(key.toLowerCase()) || item.elixir.includes(key),
     );
-    setFilterClash(filter);
+    setFilterClash2(filter);
   };
 
-  const getDeck = async () => {
-    const res = await axios.get('https://63ee4213d466e0c18bac9016.mockapi.io/deck');
+  const getDeck2 = async () => {
+    const res = await axios.get('https://63f119b05703e063fa52c282.mockapi.io/deck1');
     const data = res.data;
-    setDeck(data);
+    setDeck2(data);
     setLoaded2(true);
     console.log(res);
   };
 
   useEffect(() => {
-    getClash();
-    getDeck();
+    getClash2();
+    getDeck2();
   }, []);
 
   return (
@@ -62,7 +62,7 @@ const Deck = () => {
         </div>
         <div className="decks">
           {loaded2 ? (
-            deck.map((item) => (
+            deck2.map((item) => (
               <figure className="cr-figureDeck" key={item.id}>
                 <img src={item.iconUrls.medium} alt={item.name} />
                 <p className="cr-p1">{item.name}</p>
@@ -72,13 +72,16 @@ const Deck = () => {
                   className="btn1"
                   onClick={(ev) => {
                     ev.target.disabled = false;
-                    fetch(`https://63ee4213d466e0c18bac9016.mockapi.io/deck/${item.id}`, {
-                      method: 'DELETE',
-                      headers: {
-                        'Content-Type': 'application/json',
+                    fetch(
+                      `https://63f119b05703e063fa52c282.mockapi.io/deck1/${item.id}`,
+                      {
+                        method: 'DELETE',
+                        headers: {
+                          'Content-Type': 'application/json',
+                        },
                       },
-                    }).then(() => {
-                      getDeck();
+                    ).then(() => {
+                      getDeck2();
                     });
                   }}
                 >
@@ -104,26 +107,26 @@ const Deck = () => {
           </div>
           {loaded ? (
             <MainGallery>
-              {debouncedValue.map((character) => (
+              {debouncedValue2.map((character) => (
                 <figure className="cr-figure" key={character.id}>
                   <img src={character.iconUrls.medium} alt={character.name} />
                   <p className="cr-p1">{character.name}</p>
                   <p className="cr-p2">Level {JSON.stringify(character.maxLevel)}</p>
                   <p className="cr-p3">Elixir cost {character.elixir}</p>
-                  {deck.filter((item) => item.name === character.name).length == 0 && (
+                  {deck2.filter((item) => item.name === character.name).length == 0 && (
                     <button
                       className="btn"
                       onClick={() => {
                         {
-                          deck.length < 8
-                            ? fetch('https://63ee4213d466e0c18bac9016.mockapi.io/deck', {
+                          deck2.length < 8
+                            ? fetch('https://63f119b05703e063fa52c282.mockapi.io/deck1', {
                                 method: 'POST',
                                 headers: {
                                   'Content-Type': 'application/json',
                                 },
                                 body: JSON.stringify(character),
                               }).then(() => {
-                                getDeck();
+                                getDeck2();
                               })
                             : alert('Has alcanzado el maximo de cartas');
                         }
@@ -149,4 +152,4 @@ const Deck = () => {
   );
 };
 <h1>https://63ee4213d466e0c18bac9016.mockapi.io/deck</h1>;
-export default Deck;
+export default Deck2;
